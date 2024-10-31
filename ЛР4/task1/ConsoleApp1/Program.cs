402858725ac154e3bec373df0154d248c0e6d914
damskiiygodnik1228 <serafimgolubcov@gmail.com>
2024-10-31 12:59:09 +0300

    add

---
+++ ЛР4/task1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    // Метод для получения второй справа цифры числа
+    static int f(int x)
+    {
+        // Убедимся, что x - натуральное число
+        if (x < 10)
+        {
+            return 0; // Если число меньше 10, второй цифры нет
+        }
+
+        // Находим вторую справа цифру
+        return (x / 10) % 10;
+    }
+
+    static void Main(string[] args)
+    {
+        // Запрашиваем у пользователя ввод значений a, b и c
+        Console.Write("Введите значение a: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение c: ");
+        int c = int.Parse(Console.ReadLine());
+
+        // Вычисляем z
+        int z = f(a) + f(b) - f(c);
+
+        // Выводим результат
+        Console.WriteLine($"z = f(a) + f(b) - f(c) = {z}");
+    }
+}
---
+++ ЛР4/task2/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    // Метод для нахождения наибольшей четной цифры в числе
+    static int FindMaxEvenDigit(int number)
+    {
+        int maxEvenDigit = -1; // Инициализируем переменную для хранения максимальной четной цифры
+
+        while (number > 0)
+        {
+            int digit = number % 10; // Получаем последнюю цифру числа
+            if (digit % 2 == 0) // Проверяем, является ли цифра четной
+            {
+                if (digit > maxEvenDigit) // Если цифра больше текущей максимальной четной цифры
+                {
+                    maxEvenDigit = digit; // Обновляем максимальную четную цифру
+                }
+            }
+            number /= 10; // Убираем последнюю цифру из числа
+        }
+
+        return maxEvenDigit; // Возвращаем наибольшую четную цифру или -1, если четных нет
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество чисел (N): ");
+        int N = int.Parse(Console.ReadLine()); // Читаем количество чисел
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write($"Введите число {i + 1}: ");
+            int number = int.Parse(Console.ReadLine()); // Читаем число
+
+            int maxEvenDigit = FindMaxEvenDigit(number); // Находим максимальную четную цифру
+
+            if (maxEvenDigit == -1)
+            {
+                Console.WriteLine("В числе нет четных цифр.");
+            }
+            else
+            {
+                Console.WriteLine($"Наибольшая четная цифра в числе {number}: {maxEvenDigit}");
+            }
+        }
+    }
+}
